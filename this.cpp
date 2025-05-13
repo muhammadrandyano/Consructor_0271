1f9223455aa80123b53c5928126abdcd377a47f1
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-13 11:10:13 +0700

    membuat judul buku matematika

---
+++ this.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class buku{
+string judul;
+public :
+    string setget(string judul){
+        this->judul = judul;
+        return this->judul;
+    }     
+};
+int main()    
+{
+    buku bukunya;
+    cout<<bukunya.setget("Matematika");
+    return 0;
+}
